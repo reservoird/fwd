f763c6603fb3b00b72085dcf38a2d03469bcfddb
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-25 18:40:37 -0500

    reverting for ease of reading

--- fwd.go
+++ fwd.go
@@ -1,34 +1,19 @@
 package main
 
-import (
-	"github.com/reservoird/ibool"
-)
-
-type boolbridge struct {
-}
-
-func (o *boolbridge) Val() bool {
-	return true
-}
-
-// Fwd digester
 type fwd struct {
-	keepRunning ibool.IBool
 }
 
 // Config configures digester
 func (o *fwd) Config(cfg string) error {
-	o.keepRunning = &boolbridge{}
 	return nil
 }
 
 // Digest reads from src channel and forwards to dst channel
 func (o *fwd) Digest(src <-chan []byte, dst chan<- []byte) error {
-	for o.keepRunning.Val() == true {
+	for {
 		line := <-src
 		dst <- line
 	}
-	return nil
 }
 
 // Digester for fwd
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/reservoird/fwd
-
-go 1.13
-
-require (
-	github.com/golang/mock v1.3.1
-	github.com/reservoird/ibool v1.0.0
-)
