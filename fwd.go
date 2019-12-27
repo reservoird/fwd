3f20012d0e9b50f6fc067f17a8ef82e649c12536
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-26 21:58:05 -0500

    updating to new interface

--- fwd.go
+++ fwd.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/reservoird/reservoird/run"
+)
 
 type fwd struct {
 	run bool
@@ -11,11 +15,18 @@ func (o *fwd) Config(cfg string) error {
 	return nil
 }
 
-// Digest reads from src channel and forwards to dst channel
-func (o *fwd) Digest(src <-chan []byte, dst chan<- []byte, done <-chan struct{}, wg *sync.WaitGroup) error {
+// Digest reads from in queue and forwards to out queue
+func (o *fwd) Digest(iq run.Queue, oq run.Queue, done <-chan struct{}, wg *sync.WaitGroup) error {
 	for o.run == true {
-		line := <-src
-		dst <- line
+		d, err := iq.Pop()
+		if err != nil {
+			return err
+		}
+		err = oq.Push(d)
+		if err != nil {
+			return err
+		}
+
 		select {
 		case <-done:
 			o.run = false
