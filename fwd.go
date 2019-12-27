ba012629d9d90dd87926f27d40977f8319b8d70a
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-27 18:48:17 -0500

    adding in debug

--- fwd.go
+++ fwd.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/reservoird/icd"
@@ -22,13 +23,20 @@ func (o *fwd) Config(cfg string) error {
 
 // Digest reads from in queue and forwards to out queue
 func (o *fwd) Digest(iq icd.Queue, oq icd.Queue, done <-chan struct{}, wg *sync.WaitGroup) error {
+	defer wg.Done()
+	fmt.Printf("fwd.digest: into\n")
+
 	o.run = true
 	for o.run == true {
+		fmt.Printf("fwd.digest: before pop\n")
 		d, err := iq.Pop()
+		fmt.Printf("fwd.digest: after pop\n")
 		if err != nil {
 			return err
 		}
+		fmt.Printf("fwd.digest: before push\n")
 		err = oq.Push(d)
+		fmt.Printf("fwd.digest: after push\n")
 		if err != nil {
 			return err
 		}
@@ -39,5 +47,6 @@ func (o *fwd) Digest(iq icd.Queue, oq icd.Queue, done <-chan struct{}, wg *sync.
 		default:
 		}
 	}
+	fmt.Printf("fwd.digest: outof\n")
 	return nil
 }
