046994c39646d23b0fc9efe73c2fbd4895769af6
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-27 21:38:15 -0500

    adding in timestamp and name

--- fwd.go
+++ fwd.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"sync"
+	"time"
 
 	"github.com/reservoird/icd"
 )
 
 type fwd struct {
-	run bool
+	run       bool
+	Name      string
+	Timestamp bool
 }
 
 // NewDigester is what reservoird to create and start fwd
@@ -17,6 +23,17 @@ func NewDigester() (icd.Digester, error) {
 
 // Config configures digester
 func (o *fwd) Config(cfg string) error {
+	d, err := ioutil.ReadFile(cfg)
+	if err != nil {
+		return err
+	}
+	f := fwd{}
+	err = json.Unmarshal(d, &f)
+	if err != nil {
+		return err
+	}
+	o.Name = f.Name
+	o.Timestamp = f.Timestamp
 	return nil
 }
 
@@ -30,7 +47,15 @@ func (o *fwd) Digest(iq icd.Queue, oq icd.Queue, done <-chan struct{}, wg *sync.
 		if err != nil {
 			return err
 		}
-		err = oq.Push(d)
+		data, ok := d.([]byte)
+		if ok == false {
+			return fmt.Errorf("error invalid type")
+		}
+		line := string(data)
+		if o.Timestamp == true {
+			line = fmt.Sprintf("%s %s:", o.Name, time.Now().Format(time.RFC3339)) + line
+		}
+		err = oq.Push([]byte(line))
 		if err != nil {
 			return err
 		}
